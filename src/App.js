20bcdb14f200f2fa60c33280c8c6444f0ec7895f
patr4519 <jero96@mail.ru>
2023-03-14 14:14:31 +0300

    +

--- src/App.js
+++ src/App.js
@@ -10,18 +10,29 @@ function App() {
   const dispatch = useDispatch();
   const { items, status } = useSelector(selectMovies);
 
-  const cardList = items.map((item, index) => {
-    return <CardItem
-      title={item.Title}
-      poster={item.Poster}
-      runtime={item.Runtime}
-      country={item.Country}
-      year={item.Year}
-      genre={item.Genre}
-      ratings={item.Ratings}
-      key={index}
-    />
-  });
+  function response(items) {
+    if (items.every((obj) => obj.Response === "True")) {
+      
+      const cardList = items.map((item, index) => {
+        return (
+          <CardItem
+            title={item.Title}
+            poster={item.Poster}
+            runtime={item.Runtime}
+            country={item.Country}
+            year={item.Year}
+            genre={item.Genre}
+            ratings={item.Ratings}
+            key={index}
+          />
+        );
+      });
+
+      return cardList;
+    } else {
+      return <h2>Not found</h2>;
+    }
+  }
 
   React.useState(() => {
     dispatch(fetchMovies());
@@ -35,7 +46,7 @@ function App() {
         <div>Error</div>
       ) : (
         <div className="cardList">
-          {status === "pending" ? <div>Loading...</div> : cardList}
+          {status === "pending" ? <div>Loading...</div> : response(items)}
         </div>
       )}
     </div>
