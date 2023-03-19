53ebf6991baf372097ab01a319afde3ff0dcb10a
patr4519 <jero96@mail.ru>
2023-03-19 19:18:01 +0300

    added sort by Title function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -60,3 +60,22 @@ function Home() {
 }
 
 export default Home;
+
+
+// let arr = [
+//   {Title: 'W'},
+//   {Title: 'The Green Mile'},
+//   {Title: 'Schindler\'s List'},
+//   {Title: 'A'},
+//   {Title: 'The Shawshank Redemption'}
+//  ];
+
+//  function compareByTitle(a, b) {
+//   let titleA = a.Title.toUpperCase();
+//   let titleB = b.Title.toUpperCase();
+//   return titleA.localeCompare(titleB)
+// }
+
+// arr.sort(compareByTitle);
+
+// console.log(arr);
