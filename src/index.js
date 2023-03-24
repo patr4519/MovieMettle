7bcbaa46f629c50003c5eb17d6bcd4f899ab0e5c
patr4519 <jero96@mail.ru>
2023-03-24 15:32:51 +0300

    +

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
-
 import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
