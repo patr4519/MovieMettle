6bc78a4a85ea2c966c6b9b3110afc58d93186213
patr4519 <jero96@mail.ru>
2023-04-15 20:59:18 +0300

    +

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,8 +5,11 @@ import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchMovies } from "../../redux/slices/movieSlice";
 import { selectFavorites } from "../../redux/slices/favoritesSlice";
+import RegistrationForm from "../RegistrationForm";
 
 const Header = () => {
+  const [showSign, setShowSign] = React.useState(false);
+  const [signUp, setSignUp] = React.useState(false);
   const dispatch = useDispatch();
   const { items } = useSelector(selectFavorites);
   const favCount = items.length;
@@ -15,6 +18,18 @@ const Header = () => {
     <header>
       <div onClick={() => dispatch(fetchMovies())} className={styles.logo}>
         <Link to="/">MovieMettle</Link>
+      </div>
+      <div className={styles.signPanel}>
+        <button onClick={() => setShowSign((prev) => !prev)}>Profile</button>
+        {showSign && (
+          <div>
+            <button>Sign In</button>
+            <button onClick={() => setSignUp(prev => !prev)}>Sign Up</button>
+          </div>
+        )}
+        {
+          signUp && <RegistrationForm setSignUp={setSignUp}/>
+        }
       </div>
       <div className={styles.favorites}>
         <img width={30} className="favIcon" src={favIcon} alt="favIcon" />
@@ -26,3 +41,5 @@ const Header = () => {
 };
 
 export default Header;
+
+//  className={styles.signPanel}
---
+++ src/components/RegistrationForm/index.jsx
@@ -0,0 +1,65 @@
+import styles from "./RegistrationForm.module.scss";
+import React, { useState } from "react";
+import axios from "axios";
+
+const RegistrationForm = ({ setSignUp }) => {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLoginChange = (event) => {
+    setLogin(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  // .put("https://64116313e96e5254e2d3e6c8.mockapi.io/Users/1", {
+  //   test: 'test4'
+  // })
+  // .then(() => console.log('done'))
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("https://64116313e96e5254e2d3e6c8.mockapi.io/Users", {
+        user: { login: login, password: password },
+      });
+      setSignUp((prev) => !prev);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  return (
+    <div className={styles["form-container"]}>
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="login"
+          value={login}
+          onChange={handleLoginChange}
+          placeholder="Login"
+        />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handlePasswordChange}
+          placeholder="Password"
+        />
+        <button type="submit">Register</button>
+        <button
+          className={styles.close}
+          onClick={() => setSignUp((prev) => !prev)}
+          type="submit"
+        >
+          Close
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default RegistrationForm;
